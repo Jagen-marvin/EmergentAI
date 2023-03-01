ea94f2396c3cdf03c5e2e71a08e7d2a72a75823a
Jagen Marvin <115367924+Jagen-marvin@users.noreply.github.com>
2023-03-01 16:24:05 +0300

    emails added

--- emergentAi-master/emergentai/views.py
+++ emergentAi-master/emergentai/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse;
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 def home(request):
     return render(request,'index.html', {})
@@ -15,3 +16,26 @@ def yogera(request):
 
 def thanks(request):
     return render(request,'index-thanks.html',{})
+    
+def contact(request):
+    if requst.method == "post":
+        message_name = request.post['message-name']
+        message_email = request.post['message-email']
+        message_subject = request.post['message-subject']
+        message_number = request.post['message-number']
+        message = request.post['message']
+        
+        # send an email
+        send_mail(
+        'Message from' + message_name, # subject
+        message, # message
+        message_email, # from email
+        ['info@emergentai.ug','jagenmarvin4@gmail.com'], # to email
+        
+        )
+        
+        return render(request,'index-thanks.html',{})
+      
+     else:
+        return render(request,'index-thanks.html',{})
+        
