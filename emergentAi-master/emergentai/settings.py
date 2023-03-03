4b7da3dc766f151c3c83061736c02ccddaddfc4e
Jagen Marvin <115367924+Jagen-marvin@users.noreply.github.com>
2023-03-03 11:30:00 +0300

    email backend added

--- emergentAi-master/emergentai/settings.py
+++ emergentAi-master/emergentai/settings.py
@@ -136,3 +136,14 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'assets'),)
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# myaccount.google.com/lesssecureapps
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'emergentai.ug'
+# EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = ''
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'info@emergentai.ug'
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = 'info@emergentai.ug'
